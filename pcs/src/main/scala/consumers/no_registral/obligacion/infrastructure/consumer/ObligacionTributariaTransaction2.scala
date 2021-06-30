88d71dee00c26d8c429c5839e9c2832f87748edf
sergio.maurenzi2 <sergio.maurenzi@peperina.io>
2021-06-30 17:09:59 -0300

    more changes to use multiple DGR-COP-OBLIGACIONES-TRI topics

---
+++ pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/consumer/ObligacionTributariaTransaction2.scala
@@ -0,0 +1,61 @@
+package consumers.no_registral.obligacion.infrastructure.consumer
+
+import akka.actor.ActorRef
+import api.actor_transaction.ActorTransaction
+import api.actor_transaction.ActorTransaction.ActorTransactionRequirements
+import consumers.no_registral.obligacion.application.entities.ObligacionCommands
+import consumers.no_registral.obligacion.application.entities.ObligacionExternalDto.{DetallesObligacion, ObligacionesTri}
+import consumers.no_registral.obligacion.infrastructure.json._
+import design_principles.actor_model.Response
+import monitoring.Monitoring
+import play.api.libs.json.Reads
+import serialization.maybeDecode
+
+import scala.concurrent.Future
+import scala.util.Try
+
+case class ObligacionTributariaTransaction2(actorRef: ActorRef, monitoring: Monitoring)(
+    implicit
+    actorTransactionRequirements: ActorTransactionRequirements
+) extends ActorTransaction[ObligacionesTri](monitoring) {
+
+  def topic =
+    Try {
+      actorTransactionRequirements.config.getString(s"consumers.$simpleName.topic")
+    } getOrElse "DGR-COP-OBLIGACIONES-TRI2"
+
+  def processInput(input: String): Either[Throwable, ObligacionesTri] =
+    maybeDecode[ObligacionesTri](input)
+
+  def processMessage(registro: ObligacionesTri): Future[Response.SuccessProcessing] = {
+    implicit val b: Reads[Seq[DetallesObligacion]] = Reads.seq(DetallesObligacionF.reads)
+
+    val detalles: Option[Seq[DetallesObligacion]] = for {
+      otrosAtributos <- registro.BOB_OTROS_ATRIBUTOS
+      bobDetalles <- (otrosAtributos \ "BOB_DETALLES").toOption
+      detalles = serialization.decodeF[Seq[DetallesObligacion]](bobDetalles.toString)
+    } yield detalles
+
+    val command =
+      if (registro.BOB_ESTADO.contains("BAJA")) {
+        ObligacionCommands.DownObligacion(
+          sujetoId = registro.BOB_SUJ_IDENTIFICADOR,
+          objetoId = registro.BOB_SOJ_IDENTIFICADOR,
+          tipoObjeto = registro.BOB_SOJ_TIPO_OBJETO,
+          obligacionId = registro.BOB_OBN_ID,
+          deliveryId = registro.EV_ID
+        )
+      } else
+        ObligacionCommands.ObligacionUpdateFromDto(
+          sujetoId = registro.BOB_SUJ_IDENTIFICADOR,
+          objetoId = registro.BOB_SOJ_IDENTIFICADOR,
+          tipoObjeto = registro.BOB_SOJ_TIPO_OBJETO,
+          obligacionId = registro.BOB_OBN_ID,
+          deliveryId = registro.EV_ID,
+          registro = registro,
+          detallesObligacion = detalles.getOrElse(Seq.empty)
+        )
+    actorRef.ask[Response.SuccessProcessing](command)
+
+  }
+}
--- pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/main/ObligacionMicroservice.scala
+++ pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/main/ObligacionMicroservice.scala
@@ -1,23 +1,13 @@
 package consumers.no_registral.obligacion.infrastructure.main
 
-import scala.concurrent.ExecutionContext
-import akka.actor.{typed, ActorRef, ActorSystem}
+import akka.actor.ActorRef
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import api.actor_transaction.ActorTransaction
-import consumers.no_registral.obligacion.infrastructure.consumer.{
-  ObligacionNoTributariaTransaction,
-  ObligacionTributariaTransaction
-}
+import consumers.no_registral.obligacion.infrastructure.consumer.{ObligacionNoTributariaTransaction, ObligacionTributariaTransaction,ObligacionTributariaTransaction1, ObligacionTributariaTransaction2, ObligacionTributariaTransaction3}
 import consumers.no_registral.obligacion.infrastructure.http.ObligacionStateAPI
 import consumers.no_registral.sujeto.infrastructure.dependency_injection.SujetoActor
-import design_principles.actor_model.mechanism.QueryStateAPI.QueryStateApiRequirements
-import design_principles.actor_model.mechanism.tell_supervision.TellSupervisor
-import design_principles.microservice.kafka_consumer_microservice.{
-  KafkaConsumerMicroservice,
-  KafkaConsumerMicroserviceRequirements
-}
-import kafka.KafkaMessageProcessorRequirements
+import design_principles.microservice.kafka_consumer_microservice.{KafkaConsumerMicroservice, KafkaConsumerMicroserviceRequirements}
 
 class ObligacionMicroservice(implicit m: KafkaConsumerMicroserviceRequirements) extends KafkaConsumerMicroservice {
 
@@ -26,6 +16,9 @@ class ObligacionMicroservice(implicit m: KafkaConsumerMicroserviceRequirements) 
   override def actorTransactions: Set[ActorTransaction[_]] =
     Set(
       ObligacionTributariaTransaction(actor, monitoring),
+      ObligacionTributariaTransaction1(actor, monitoring),
+      ObligacionTributariaTransaction2(actor, monitoring),
+      ObligacionTributariaTransaction3(actor, monitoring),
       ObligacionNoTributariaTransaction(actor, monitoring)
     )
 
