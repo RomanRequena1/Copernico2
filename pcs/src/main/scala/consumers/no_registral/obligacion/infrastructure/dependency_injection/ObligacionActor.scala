7104e135d04fb6cf6b226896f05e285db1b97604
Manuel Baraschi <manuel.baraschi@peperina.io>
2021-08-26 17:48:57 +0000

    remove  unnecessary   println

--- pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/dependency_injection/ObligacionActor.scala
+++ pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/dependency_injection/ObligacionActor.scala
@@ -62,8 +62,6 @@ class ObligacionActor(requirements: MonitoringAndMessageProducer)
   import consumers.no_registral.obligacion.infrastructure.json._
   def persistSnapshot()(handler: () => Unit): Unit = {
 
-    println("")
-
     val ids = ObligacionMessageRoots.extractor(persistenceId)
 
     val kafkaTopic = "ObligacionPersistedSnapshot"
