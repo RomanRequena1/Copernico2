c37cba8ae7774e1f163c19c588ca574f779adf23
Lucas Paninka <lucas.paninka@peperina.io>
2021-11-18 11:06:42 -0300

    cant_cuotas_pagadas added

--- readside/src/main/scala/readside/proyectionists/no_registrales/objeto/projections/ObjetoSnapshotPersistedProjection.scala
+++ readside/src/main/scala/readside/proyectionists/no_registrales/objeto/projections/ObjetoSnapshotPersistedProjection.scala
@@ -17,7 +17,8 @@ case class ObjetoSnapshotPersistedProjection(
       "soj_id_externo" -> r.SOJ_ID_EXTERNO,
       "soj_otros_atributos" -> r.SOJ_OTROS_ATRIBUTOS,
       "soj_base_imponible" -> r.SOJ_BASE_IMPONIBLE,
-      "soj_adherido_debito" -> r.SOJ_ADHERIDO_DEBITO
+      "soj_adherido_debito" -> r.SOJ_ADHERIDO_DEBITO,
+      "soj_cant_cuotas_pagadas" -> r.SOJ_CANT_CUOTAS_PAGADAS
     ))
     case None => Some(List(
       "soj_cat_soj_id" -> None,
@@ -28,7 +29,8 @@ case class ObjetoSnapshotPersistedProjection(
       "soj_id_externo" -> None,
       "soj_otros_atributos" -> None,
       "soj_base_imponible" -> None,
-      "soj_adherido_debito" -> None
+      "soj_adherido_debito" -> None,
+      "soj_cant_cuotas_pagadas" -> None
     ))
   }
 
