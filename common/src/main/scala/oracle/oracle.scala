8cf72ff57dc68e9569e8c0840bb0965a0a4fa7ac
lautaro.bustos <lautaro.bustos@peperina.io>
2022-11-15 15:02:22 -0300

    add traz in all consumer

--- common/src/main/scala/oracle/oracle.scala
+++ common/src/main/scala/oracle/oracle.scala
@@ -1,10 +1,13 @@
 package oracle
 import java.sql.Connection
 import _root_.oracle.jdbc.pool.OracleDataSource
+import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.LoggerFactory
 
 object oracle {
-
+  private val config: Config = ConfigFactory.load()
+  val ip = config.getString("oracle.ip")
+  val port = config.getString("oracle.port")
   private val log = LoggerFactory.getLogger(this.getClass)
   def connOracleKafkaToWriteside(ev_id: String, entidad: String, bob_canal_origen: String) = {
 
@@ -23,10 +26,9 @@ object oracle {
            set PASO = '05'
            where EV_ID = '${ev_id}' and BOB_CANAL_ORIGEN = '${bob_canal_origen}'
     """
-    val connection : Connection = null
     val oracleUser = "usrnifi"
     val oraclePassword = "usrnifi"
-    val oracleURL = "jdbc:oracle:thin:@10.250.11.15:1521/PTAXDESA"
+    val oracleURL = s"jdbc:oracle:thin:@${ip}:${port}/PTAXDESA"
     val ods = new OracleDataSource()
     ods.setUser(oracleUser)
     ods.setURL(oracleURL)
@@ -61,10 +63,9 @@ object oracle {
            where EV_ID = '${ev_id}' and BOB_CANAL_ORIGEN = '${bob_canal_origen}'
     """
 
-    val connection : Connection = null
     val oracleUser = "usrnifi"
     val oraclePassword = "usrnifi"
-    val oracleURL = "jdbc:oracle:thin:@10.250.11.15:1521/PTAXDESA"
+    val oracleURL = s"jdbc:oracle:thin:@${ip}:${port}/PTAXDESA"
     val ods = new OracleDataSource()
     ods.setUser(oracleUser)
     ods.setURL(oracleURL)
