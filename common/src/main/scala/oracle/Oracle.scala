793a7f339f4b42e7450cdf7ef92625caae67445f
Lucas Paninka <lucas.paninka@peperina.io>
2022-11-16 16:19:24 -0300

    Unstable

---
+++ common/src/main/scala/oracle/Oracle.scala
@@ -0,0 +1,93 @@
+package oracle
+import _root_.oracle.jdbc.pool.OracleDataSource
+import com.typesafe.config.{Config, ConfigFactory}
+import org.slf4j.LoggerFactory
+
+object Oracle {
+  private val config: Config = ConfigFactory.load()
+  val url = config.getString("oracle.url")
+  val user = config.getString("oracle.user")
+  val password = config.getString("oracle.password")
+
+  private val log = LoggerFactory.getLogger(this.getClass)
+
+  val oracleUser = s"${user}"
+  val oraclePassword = s"${password}"
+  val oracleURL = s"${url}"
+  val ods = new OracleDataSource()
+  ods.setUser(oracleUser)
+  ods.setURL(oracleURL)
+  ods.setPassword(oraclePassword)
+
+
+  def connOracleKafkaToWriteside(ev_id: String, entidad: String, bob_canal_origen: String) = {
+    try {
+      val con = ods.getConnection()
+      val statement = con.createStatement()
+      val queryObligacion =
+        s"""
+           update tax.EVENTOS_OBN_LOGS
+           set PASO = '05'
+           where EV_ID = '${ev_id}' and BOB_CANAL_ORIGEN = '${bob_canal_origen}'
+    """
+      val queryObjeto =
+        s"""
+           update tax.EVENTOS_OBN_LOGS
+           set PASO = '05'
+           where EV_ID = '${ev_id}' and BOB_CANAL_ORIGEN = '${bob_canal_origen}'
+    """
+      val querySujeto =
+        s"""
+           update tax.EVENTOS_OBN_LOGS
+           set PASO = '05'
+           where EV_ID = '${ev_id}' and BOB_CANAL_ORIGEN = '${bob_canal_origen}'
+    """
+
+      statement.setFetchSize(1000) // important
+      entidad match {
+        case x if x == "obligacion" => statement.executeUpdate(queryObligacion)
+        case x if x == "objeto" => statement.executeUpdate(queryObjeto)
+        case x if x == "sujeto" => statement.executeUpdate(querySujeto)
+        case _ => log.error("Dont exist this entity")
+      }
+    } catch {
+      case e: Exception => log.error("Error connection to Oracle - " + e)
+    }
+  }
+
+  def connOracleReadsideToCass(ev_id: String,  entidad: String, bob_canal_origen:String) = {
+    try {
+      val con = ods.getConnection()
+      val statement = con.createStatement()
+      val queryObligacion =
+        s"""
+           update tax.EVENTOS_OBN_LOGS
+           set PASO = '06'
+           where EV_ID = '${ev_id}' and BOB_CANAL_ORIGEN = '${bob_canal_origen}'
+    """
+      val queryObjeto =
+        s"""
+           update tax.EVENTOS_OBN_LOGS
+           set PASO = '06'
+           where EV_ID = '${ev_id}' and BOB_CANAL_ORIGEN = '${bob_canal_origen}'
+    """
+      val querySujeto =
+        s"""
+           update tax.EVENTOS_OBN_LOGS
+           set PASO = '06'
+           where EV_ID = '${ev_id}' and BOB_CANAL_ORIGEN = '${bob_canal_origen}'
+    """
+
+      entidad match {
+        case x if x == "obligacion" => statement.executeUpdate(queryObligacion)
+        case x if x == "objeto" => statement.executeUpdate(queryObjeto)
+        case x if x == "sujeto" => statement.executeUpdate(querySujeto)
+        case _ => log.error("Dont exist this entity")
+      }
+    } catch {
+      case e: Exception => log.error("Error connection to Oracle - " + e)
+    }
+  }
+
+}
+
