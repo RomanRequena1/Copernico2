a2d7d53aca3290dc0b7bf92a95dd9acf74ecc6c0
Diego Rivera <roman.requena@peperina.io>
2024-06-28 16:09:04 -0300

    Partial state: change to null to delete value from state

--- pcs/src/main/scala/consumers/no_registral/sujeto/application/cqrs/commands/SujetoUpdateFromTriHandler.scala
+++ pcs/src/main/scala/consumers/no_registral/sujeto/application/cqrs/commands/SujetoUpdateFromTriHandler.scala
@@ -29,7 +29,7 @@ class SujetoUpdateFromTriHandler(actor: SujetoActor) extends SyncCommandHandler[
         campoEstado.setAccessible(true)
         if (campoEvento.get(evento) == None) {
           campoEvento.set(sujetoNuevoTest, campoEstado.get(estado))
-        } else if (campoEvento.get(evento).equals(Some(""))) {
+        } else if (campoEvento.get(evento).equals(Some("null"))) {
           campoEvento.set(sujetoNuevoTest, None)
         }
       }
