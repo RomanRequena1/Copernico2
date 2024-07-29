895a4a255997e5c8f1ecb27b876e7e02aec55e98
Lucas Paninka <lucas.paninka@peperina.io>
2024-07-29 16:02:18 -0300

    Deleted println

--- pcs/src/main/scala/consumers/no_registral/tranferencia/application/cqrs/commands/UpdateObjetoVinculoFromObjHandler.scala
+++ pcs/src/main/scala/consumers/no_registral/tranferencia/application/cqrs/commands/UpdateObjetoVinculoFromObjHandler.scala
@@ -36,7 +36,7 @@ class UpdateObjetoVinculoFromObjHandler(actor: ObjetoVinculoActor, tranferenciaA
       actor.state += event
       actor.state.mapVinculo.foreach {
         e => {
-          println(s"Objeto Persist desde Map: ${e._1.objetoId} | Sujeto: ${e._1.sujetoId} | Tiene30Objeto = ${actor.state.tiene30ObjetoVinculo}")
+          //println(s"Objeto Persist desde Map: ${e._1.objetoId} | Sujeto: ${e._1.sujetoId} | Tiene30Objeto = ${actor.state.tiene30ObjetoVinculo}")
             actorSujetoGeneral.ask[Response.SuccessProcessing](UpdateState30ObjetoFromObjVinculo(0, e._1.sujetoId, e._1.objetoId, e._1.tipoObj, actor.state.tiene30ObjetoVinculo, command.exclusionObjeto.get))
         }
       }
