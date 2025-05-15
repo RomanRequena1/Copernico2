78c91b068371e56423c723567f0a312b9382a271
Roman Requena <roman.requena@peperina.io>
2025-05-15 12:43:22 -0300

    ordenamiento obligaciones tri y ant

--- pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/consumer/ObligacionNoTributariaTransaction.scala
+++ pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/consumer/ObligacionNoTributariaTransaction.scala
@@ -1,42 +1,34 @@
 package consumers.no_registral.obligacion.infrastructure.consumer
-import akka.actor.ActorRef
+import akka.actor.{ActorRef, ActorSystem}
+import akka.util.Timeout
 import api.actor_transaction.ActorTransaction
 import api.actor_transaction.ActorTransaction.ActorTransactionRequirements
-import consumers.no_registral.obligacion.application.dmn.DMNTreintaPorciento
-import consumers.no_registral.obligacion.application.entities
-import consumers.no_registral.obligacion.application.entities.ObligacionCommands.{
-  ObligacionAntUpdateFromDto,
-  ObligacionRemove,
-  ObligacionUpdateFromDto
-}
-import consumers.no_registral.obligacion.application.entities.{
-  DetallesObligacion,
-  DetallesSupresiones,
-  ListDetallesObligaciones,
-  ObligacionCommands,
-  ObligacionesAnt
-}
+import consumers.no_registral.obligacion.application.entities.ObligacionCommands.ObligacionAntUpdateFromDto
+import consumers.no_registral.obligacion.application.entities._
 import consumers.no_registral.obligacion.infrastructure.json.ObligacionImplicits._
+import consumers.no_registral.obligacion.infrastructure.sorter.ObligacionCommandRouter
 import design_principles.actor_model.Response
 import io.circe.parser.decode
-import io.circe.syntax.EncoderOps
 import monitoring.Monitoring
-import org.camunda.dmn.DmnEngine
-import org.camunda.dmn.parser.ParsedDmn
-import scalaz.\/
 
-import java.io.FileInputStream
-import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 case class ObligacionNoTributariaTransaction(actorRef: ActorRef, monitoring: Monitoring)(
     implicit
-    actorTransactionRequirements: ActorTransactionRequirements
+    actorTransactionRequirements: ActorTransactionRequirements,
+    system: ActorSystem
 ) extends ActorTransaction[ObligacionesAnt](monitoring) {
 
+  implicit val timeout: Timeout = Timeout(30.seconds)
+  implicit val ec: ExecutionContext = actorTransactionRequirements.executionContext
+
+  val sorterEnabled: String = Try(System.getenv("BETTER_SORTER")).getOrElse("OFF")
+  private val commandRouter = ObligacionCommandRouter.getOrCreate(system, actorRef)
+
   def topic = "DGR-COP-OBLIGACIONES-ANT"
-
   def topicRetry = "DGR-COP-OBLIGACIONES-ANT_retry"
-
   def topicError = "DGR-COP-OBLIGACIONES-ANT_error"
 
   def processInput(input: String): Either[Throwable, ObligacionesAnt] = {
@@ -109,7 +101,13 @@ case class ObligacionNoTributariaTransaction(actorRef: ActorRef, monitoring: Mon
             cuota = obligacion.BOB_CUOTA
           )
         }
-      actorRef.ask[Response.SuccessProcessing](command)
+      sorterEnabled.equals("ON") match {
+        case true => {
+          recordBetterSorter()
+          commandRouter.ask[Response.SuccessProcessing](command)
+        }
+        case false => actorRef.ask[Response.SuccessProcessing](command)
+      }
     }
   }
 }
