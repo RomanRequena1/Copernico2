9ba10bcc568b2ea7423457fdb8acb94cf71059fc
Julian Puppato <julian.puppato@peperina.io>
2026-02-04 15:52:54 -0300

    fix obligacion remove

--- pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/consumer/ObligacionTributariaTransactionIngresoBruto.scala
+++ pcs/src/main/scala/consumers/no_registral/obligacion/infrastructure/consumer/ObligacionTributariaTransactionIngresoBruto.scala
@@ -38,6 +38,10 @@ case class ObligacionTributariaTransactionIngresoBruto(actorRef: ActorRef, monit
   }
 
   def processMessage(obligacion: ObligacionesTri): Future[Response.SuccessProcessing] = {
+    val dmn = isTreintaPorciento(obligacion)
+    val dmnResultTuple = dmn._2
+    val dmnNumero = dmnResultTuple._1
+    val dmnDescripcion = dmnResultTuple._2
     val isNotDeuda: Option[ListDetallesObligaciones] => List[Boolean] = {
       case Some(d) =>
         d.BOB_DETALLES map { d =>
@@ -80,7 +84,8 @@ case class ObligacionTributariaTransactionIngresoBruto(actorRef: ActorRef, monit
             tipoObjeto = obligacion.BOB_SOJ_TIPO_OBJETO,
             obligacionId = obligacion.BOB_OBN_ID,
             registro = obligacion,
-            cuota = obligacion.BOB_CUOTA
+            cuota = obligacion.BOB_CUOTA,
+            resultDmn = Some(s"($dmnNumero,$dmnDescripcion)")
           )
         } else if (isNotDeuda(obligacion.BOB_OTROS_ATRIBUTOS).head) {
           ObligacionCommands.ObligacionRemove(
@@ -90,7 +95,8 @@ case class ObligacionTributariaTransactionIngresoBruto(actorRef: ActorRef, monit
             tipoObjeto = obligacion.BOB_SOJ_TIPO_OBJETO,
             obligacionId = obligacion.BOB_OBN_ID,
             registro = obligacion,
-            cuota = obligacion.BOB_CUOTA
+            cuota = obligacion.BOB_CUOTA,
+            resultDmn = Some(s"($dmnNumero,$dmnDescripcion)")
           )
         } else {
           val dmn = isTreintaPorciento(obligacion)
